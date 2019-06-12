b7cc57e83d79791dc047691de0c6cc302ca28b13
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-06-12 01:06:07 +0100

    update app

--- src/config/static.config.js
+++ src/config/static.config.js
@@ -1,7 +1,7 @@
 const staticFiles = {
     logo : "/img/logooo.png",
     css : "/style/style.css" ,
-    bootstrap : "/style/bootstrap.min.css",
+    bootstrap : "/style/bootstrap.css",
     fontawesome : "/style/all.min.css",
     js : "/js/custom.js"
 }
