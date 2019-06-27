fc6bd915b7bc2497fb2b37f90555a4c4b569936c
Haikel Fazzani <haikel.fazzani@hotmail.com>
2019-06-27 11:48:26 +0100

    update app

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -1,18 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const btnDrop = document.getElementById("navbarDropdownMenuLink-4");
-    const menuDropDown = document.querySelector(".dropdown-menu");
-
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const collapse = document.querySelector(".collapse");
-
-    function toggle(parent, child) {
-        parent.onclick = () => {
-            child.style.display = child.style.display === "block" ? "none" : "block";
-        }
-    }
-
-    toggle(btnDrop, menuDropDown);
-    toggle(navbarToggler, collapse);    
+        
 
 });
